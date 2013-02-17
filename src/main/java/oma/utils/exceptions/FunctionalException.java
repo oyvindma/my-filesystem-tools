1bd39e2185c5a2450558485f5137ad469fbdf3e4
Øyvind Møller Asbjørnsen <oyvindma@gmail.com>
2013-02-17 23:54:28 +0100

    Created project and file repository. FileRepository has method to return a list of files in a folder.

---
+++ src/main/java/oma/utils/exceptions/FunctionalException.java
@@ -0,0 +1,10 @@
+package oma.utils.exceptions;
+public class FunctionalException extends RuntimeException{
+	
+    private static final long serialVersionUID = 1L;
+
+	public FunctionalException(String message) {
+		super(message);
+    }
+	
+}
---
+++ src/test/java/oma/utils/repository/FileRepositoryTest.java
@@ -0,0 +1,67 @@
+package oma.utils.repository;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import oma.utils.exceptions.FunctionalException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+public class FileRepositoryTest {
+	
+	@Rule
+	public TemporaryFolder sourceFolder = new TemporaryFolder();
+	private FileRepository repository;
+	
+	@Before
+	public void setUp() throws IOException{
+		sourceFolder.create();
+		repository = new FileRepository();
+		
+	}
+
+	@Test
+	public void shallReturnAllFilesInADirectory() throws IOException {
+		sourceFolder.newFile("a.txt");
+		
+		List<File> filesFound = repository.findFilesInDirectory(sourceFolder.getRoot());
+		
+		assertThat(filesFound)
+						.isNotNull()
+						.isNotEmpty()
+						.contains(new File(sourceFolder.getRoot(), "a.txt"));
+	}
+	
+	@Test
+	public void shallReturnEmptyListWhenNoFilesFound() throws IOException {
+		
+		List<File> filesFound = repository.findFilesInDirectory(sourceFolder.getRoot());
+		
+		assertThat(filesFound)
+						.isNotNull()
+						.isEmpty();
+	}
+	
+	@Test
+	public void shallThrowExceptionWhenSourceFolderDoesNotExist() throws IOException {
+		sourceFolder.delete();
+		assertThat(sourceFolder.getRoot()).doesNotExist();
+		
+		try{
+			repository.findFilesInDirectory(sourceFolder.getRoot());
+			fail("Should throw exception when source folder does not exist.");
+		}catch(Exception e){
+			assertThat(e).isInstanceOf(FunctionalException.class);
+		}
+		
+	}
+
+}
+
+
