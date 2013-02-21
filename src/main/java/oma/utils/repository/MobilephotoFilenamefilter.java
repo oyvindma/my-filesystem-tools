35e8bebad41d2c999e28048a0e431676019e1fc8
Øyvind Møller Asbjørnsen <oyvindma@gmail.com>
2013-02-21 21:26:56 +0100

    Renamed MobilePhotoFileNameFilter

--- src/main/java/oma/utils/repository/MobilephotoFilenamefilter.java
+++ src/main/java/oma/utils/repository/MobilephotoFilenamefilter.java
@@ -3,11 +3,12 @@ package oma.utils.repository;
 import java.io.File;
 import java.io.FilenameFilter;
 
-public class MobilePhotoFileNameFilter implements FilenameFilter{
+public class MobilephotoFilenamefilter implements FilenameFilter{
 	String fileNamePatternA="\\d{4}-\\d{2}-\\d{2} \\d{2}.\\d{2}.\\d{2}(-\\d+)?.jpg";
 	String fileNamePatternB="IMG_\\d{8}_\\d{6}.jpg";
 	@Override
     public boolean accept(File dir, String name) {
+		
 	    return name.matches(fileNamePatternA) || name.matches(fileNamePatternB);
     }
 	
--- src/test/java/oma/utils/repository/MobilephotoFilenamefilterTest.java
+++ src/test/java/oma/utils/repository/MobilephotoFilenamefilterTest.java
@@ -1,16 +1,16 @@
-package oma.utils.filrydding;
+package oma.utils.repository;
 
 import static oma.utils.filrydding.ObjectMother.dummyFolder;
 import static oma.utils.filrydding.ObjectMother.fileName_2010_08_28_18_19_19;
 import static oma.utils.filrydding.ObjectMother.fileName_2010_08_28_18_19_19_1;
 import static oma.utils.filrydding.ObjectMother.fileName_IMG_20100828_181919;
 import static org.fest.assertions.Assertions.assertThat;
-import oma.utils.repository.MobilePhotoFileNameFilter;
+import oma.utils.repository.MobilephotoFilenamefilter;
 
 import org.junit.Test;
 
-public class MobilePhotoFileNameFilterTest {
-	private MobilePhotoFileNameFilter filter = new MobilePhotoFileNameFilter();
+public class MobilephotoFilenamefilterTest {
+	private MobilephotoFilenamefilter filter = new MobilephotoFilenamefilter();
 	
 	@Test
 	public void shallAcceptFileNameFollowingPatternYYYY_mm_DD_HH_MM_SS_jpg(){
