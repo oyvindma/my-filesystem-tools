6d28a422d241951c2face6361c16c76ec0e5d3c4
Øyvind Møller Asbjørnsen <oyvindma@gmail.com>
2013-03-04 00:05:05 +0100

    Threw something together to find the files and move the ones to keep to another directory.

--- src/main/java/oma/utils/MyApp.java
+++ src/main/java/oma/utils/MyApp.java
@@ -2,40 +2,42 @@ package oma.utils;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
+
+import com.google.common.io.Files;
 
 import oma.utils.filrydding.FileService;
 import oma.utils.filrydding.FilenameTransformer;
-import oma.utils.filrydding.domain.Filegroups;
+import oma.utils.filrydding.domain.FileGroup;
+import oma.utils.filrydding.domain.FileGroups;
 import oma.utils.filrydding.domain.PictureFile;
 import oma.utils.repository.FileRepositoryImpl;
 import oma.utils.repository.MobilephotoFilenamefilter;
 
 public class MyApp {
-	public static void main(String[] args) {
-	    FileService fileFinderService = new FileService(new FileRepositoryImpl());
-	    File parentDirectory = new File("/home/oyvindma/Pictures/mobilbilder");
+	public static void main(String[] args) throws IOException {
+		FileService fileFinderService = new FileService(new FileRepositoryImpl());
+		File parentDirectory = new File("/home/oyvindma/Pictures/mobilbilder");
 		FilenameFilter filenameFilter = new MobilephotoFilenamefilter();
 		FilenameTransformer transformer = new FilenameTransformer();
-		
-		Filegroups filesGroupedByTransformedFilename = fileFinderService.getFilesGroupedByTransformedFilename(parentDirectory, filenameFilter, transformer);
 
-		//TODO create Filegroup to hold List<PictureFile> and methods to extract relevant data.
-		Map<String, List<PictureFile>> groupsWithMoreThanOneFile = filesGroupedByTransformedFilename.getGroupsWithMoreThanOneFile();
-		
-		Iterator<Entry<String, List<PictureFile>>> iterator = groupsWithMoreThanOneFile.entrySet().iterator(); 
-		while (iterator.hasNext()){
-			Entry<String, List<PictureFile>> entry = iterator.next();
-			System.out.println("Entry: "+entry.getKey());
-			List<PictureFile> files = entry.getValue();
-			for(PictureFile picture: files){
-				System.out.println(picture.getFileName());
+		FileGroups filesGroupedByTransformedFilename = fileFinderService.getFilesGroupedByTransformedFilename(parentDirectory,
+		        filenameFilter, transformer);
+
+		FileGroups groupsWithMoreThanOneFile = filesGroupedByTransformedFilename.getGroupsWithMoreThanOneFile();
+
+		Iterator<Entry<String, FileGroup<PictureFile>>> iterator = filesGroupedByTransformedFilename.entrySet().iterator();
+		StringBuilder filesToKeep = new StringBuilder();
+		File target = new File("mob/files-to-keep/");
+			while (iterator.hasNext()) {
+				Entry<String, FileGroup<PictureFile>> entry = iterator.next();
+				File largestFile = entry.getValue().getLargestFile();
+//				Files.move(largestFile, new File(target.getAbsolutePath(), largestFile.getName()));
 			}
-        }
-		
-    }
+		System.out.println("Files with duplicates:" + groupsWithMoreThanOneFile.size());
+		System.out.println("Files to keep:" + filesGroupedByTransformedFilename.size());
+	}
 
 }
